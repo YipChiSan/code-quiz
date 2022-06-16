388b594a5ebf40007550af9a8e1c49ff11fba8b2
YipChiSan <zye39@yahoo.com>
2022-06-16 21:14:21 +1000

    Finish Clear Button

--- assets/js/highScoreScript.js
+++ assets/js/highScoreScript.js
@@ -4,3 +4,8 @@ backBtnEl.addEventListener('click', handleBackBtn);
 function handleBackBtn() {
     location.href = 'index.html'
 }
+
+document.querySelector('#clear').addEventListener('click', function() {
+    localStorage.clear();
+    document.querySelector('ul').innerHTML = '';
+});
